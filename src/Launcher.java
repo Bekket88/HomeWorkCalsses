810ba2fab3c17d59462b16584db4a48a521a1061
Vladimir Popov <Bekket88@gmail.com>
2022-04-24 12:52:36 +0300

    Готовое ДЗ по классам и объектам (средний уровень)

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,12 +1,32 @@
+import java.util.ArrayList;
+
 public class Launcher {
 
-    public static void main (String[] strings) {
+    public static void addBook(Book[] books, Book addBook) {
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] == null) {
+                books[i] = addBook;
+                break;
+            }
+        }
+    }
+    public static void printBooks(Book[] books) {
+        for (Book book : books) {
+            if (book != null) {
+                System.out.println(book.getAuthor().getFirstName() + " " + book.getAuthor().getLastName() + ": " + book.getBookName() + ": " + book.getReleaseYear());
+            }
+        }
+    }
+
+    public static void main(String[] strings) {
 
         Book witcher = new Book("Ведьмак", 1990, new Author("Анджей", "Сапковский"));
         witcher.setReleaseYear(1986);
-        System.out.println(witcher.getBookName() + ", " + witcher.getAuthor().getFirstName() + " " + witcher.getAuthor().getLastName() + ", " + witcher.getReleaseYear());
+        Book dune = new Book("Дюна", 1965, new Author("Френк", "Герберт"));
 
-        Book dune = new Book ("Дюна", 1965, new Author("Френк", "Герберт"));
-        System.out.println(dune.getBookName() + ", " + dune.getAuthor().getFirstName() + " " + dune.getAuthor().getLastName() + ", " + dune.getReleaseYear());
+        Book[] books = new Book[5];
+        addBook(books, witcher);
+        addBook(books, dune);
+        printBooks(books);
     }
 }
